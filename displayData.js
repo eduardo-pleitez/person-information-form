f3ee26bf24e3c4b452c23bc535429fcc169a5388
Eduardo Mejia Pleitez <eamejia@applaudostudios.dev>
2022-07-29 16:41:23 -0600

    Function to display data added and regex for pass updated

---
+++ displayData.js
@@ -0,0 +1,16 @@
+
+function showData(successList, errorList) {
+
+  let displayList = '<h2>Fix the following errors:</h2> <ul> \n';
+  let errorListContainer = document.getElementById('errorListContainer');
+
+  //Display the valid information
+  console.log(successList);
+
+  //Display the errors
+  if(errorList.length > 0){
+    errorList.map((errorItem) => displayList += '<li>' + errorItem + '</li> \n');
+    displayList += '</ul>';
+    errorListContainer.innerHTML = displayList;
+  }
+}
--- formValidation.js
+++ formValidation.js
@@ -1,3 +1,4 @@
+
 /* Get submit buttton and add event listener after click*/
 const submit = document.getElementById('submitBtn');
 submit.addEventListener('click', formValidation);
@@ -24,7 +25,7 @@ function formValidation(e){
   /* Create the regular expressions to validate inputs */
   const regExpText = /^[A-Za-zàèìòùÀÈÌÒÙáéíóúýÁÉÍÓÚÝâêîôûÂÊÎÔÛãñõÃÑÕäëïöüÿÄËÏÖÜŸçÇßØøÅåÆæœ'`']+(?:\s[a-zA-ZàèìòùÀÈÌÒÙáéíóúýÁÉÍÓÚÝâêîôûÂÊÎÔÛãñõÃÑÕäëïöüÿÄËÏÖÜŸçÇßØøÅåÆæœ'`']+)*$/;
   const regExpEmail = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-  const regExpPass = /^[A-Za-z0-9#?!@$%^&*-]{8,}$/;
+  const regExpPass = /^[A-Za-z0-9#?!@$%^&*-.,_¿¡;]{8,}$/;
   const regExpPhone = /^[0-9]{4}\-?[0-9]{4}$/;
   const regExpWebsite = /^((ftp|http|https):\/\/)?(www.)?(?!.*(ftp|http|https|www.))[a-zA-Z0-9_-]+(\.[a-zA-Z]+)+((\/)[\w#]+)*(\/\w+\?[a-zA-Z0-9_]+=\w+(&[a-zA-Z0-9_]+=\w+)*)?\/?$/gm;
 
@@ -34,19 +35,19 @@ function formValidation(e){
 
   /* Checking input values */
   //Input name
-  (pName.value.match(regExpText) && pName.value.length>2) ? successList += 'Name: ' + pName.value.toUpperCase() + '\n' : errorList.push('The name must contain just letters.');
+  (pName.value.match(regExpText) && pName.value.length>2) ? successList += 'Name: ' + pName.value.toUpperCase() + '\n' : errorList.push('The name must be letters.');
 
   //Input last name
-  (pLastName.value.match(regExpText) && pName.value.length>2) ? successList += 'Last Name: ' + pLastName.value.toUpperCase() + '\n' : errorList.push('The last name must contain just letters.');
+  (pLastName.value.match(regExpText) && pName.value.length>2) ? successList += 'Last Name: ' + pLastName.value.toUpperCase() + '\n' : errorList.push('The last name must be letters.');
 
   //Input email
   (email.value.match(regExpEmail)) ? successList += 'E-mail: ' + email.value + '\n' : errorList.push('Insert a correct e-mail address.');
 
   //Input password and input password confirmation
-   (emailPass.value.match(regExpPass) && emailPass.value == emailConfPass.value) ? successList += 'E-mail password: ' + emailPass.value + '\n' : errorList.push('The password and the confirmation password don\'t match or '); 
+   (emailPass.value.match(regExpPass) && emailPass.value == emailConfPass.value) ? successList += 'E-mail password: ' + emailPass.value + '\n' : errorList.push('The password and the confirmation password don\'t match or is not 8 charaters long.'); 
  
   //Input phone
-  (phone.value.match(regExpPhone)) ? successList += 'Phone number: ' + phone.value + '\n' : errorList.push('The phone number must contain 8 digits.');
+  (phone.value.match(regExpPhone)) ? successList += 'Phone number: ' + phone.value + '\n' : errorList.push('The phone number must be 8 digits long.');
 
   //Input age
   (Number(age.value) >= 18 && Number(age.value) <= 120) ? successList += 'Age: ' + age.value + '\n' : errorList.push('Age must be a number between 18 and 120');
@@ -55,7 +56,7 @@ function formValidation(e){
   (gender != null) ? successList += 'Gender: ' + gender.value + '\n' : errorList.push('Select a gender option.');
 
   //Input interests
-  (interests.length >= 2) ? Array.from(interests).map(interest => interestsList += interest.value + '\n') : errorList.push('Must select minimum 2 interests.');
+  (interests.length >= 2) ? Array.from(interests).map(interest => interestsList += interest.value + '\n') : errorList.push('Must select at least 2 interests.');
   //Add the interests to the successList just if the clause is valid.
   (interests.length >= 2) ? successList += interestsList : null; 
 
@@ -64,5 +65,6 @@ function formValidation(e){
 
   //Add experience level
   (Number(expLvl.value) < 34) ? successList += 'Experience level: Junior \n' : (Number(expLvl.value) > 66) ? successList += 'Experience level: Senior \n' : successList += 'Experience level: Mid-level \n';
-  
+
+  showData(successList, errorList);
 }
